02d511cf2bdda9f2aa6ab7685a3dcaf7329d09c9
PiotrJaromin <piotrek.jaromin@gmail.com>
2018-01-12 13:40:01 +0100

    first commit

---
+++ db/migrate/20180112122653_create_tickets.rb
@@ -0,0 +1,14 @@
+class CreateTickets < ActiveRecord::Migration
+  def change
+    create_table :tickets do |t|
+      t.string :name
+      t.string :seat_id_seq
+      t.text :address
+      t.decimal :price
+      t.string :email_address
+      t.string :phone
+
+      t.timestamps null: false
+    end
+  end
+end
